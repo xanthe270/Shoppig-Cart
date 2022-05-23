5e7c510b4c693f0067f145e28a8615b2edc126c9
xanthe270 <90760206+xanthe270@users.noreply.github.com>
2022-05-23 20:11:47 +0200

    Add files via upload

---
+++ SC.js
@@ -0,0 +1,88 @@
+var nums = [1, 2, 3, 4];
+var items = ["Coke", "Kit Kat", "Bar One", "Fanta"];
+var prices = [7.5, 9.5, 8.5, 7.5];
+var quantities = [0, 0, 0, 0];
+var totals = [0.0, 0.0, 0.0, 0.0];
+var totalOrderAmt = 0;
+
+
+
+function add_selection(x) {
+    console.log(x);
+    quantities[x] = quantities[x] + 1;
+    totals[x] = prices[x] * quantities[x];
+    totalOrderAmt += prices[x];
+
+    display_all();
+
+
+}
+
+function minus_selection(x) {
+
+    if (quantities[x] <= 0) {
+
+        
+        return alert("Add before Minus!");
+		display_all();
+
+    } else if (quantities[x] > 0) {
+        console.log(x);
+		
+        quantities[x] = quantities[x] - 1;
+        totals[x] = prices[x] * quantities[x];
+        totalOrderAmt -= prices[x];
+
+        display_all();
+    }
+}
+
+
+function displayTotal() {
+
+    document.getElementById("total").innerHTML += "<br/><br/>Total order amount is R" + totalOrderAmt.toFixed(2);
+}
+
+function display_Total ()
+{
+	return alert("Your Total is:" + totalOrderAmt);
+}
+
+
+function display_all() {
+
+
+
+    var myTable = "<table><th style='width: 100px; color: red; text-align: right;'>Num</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Item</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Price</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Quantity</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Total</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Add</th>";
+    myTable += "<th style='width: 100px; color: red; text-align: right;'>Minus</th>";
+	myTable += "<th style='width: 100px; color: red; text-align: right;'>Calculate</th>";
+
+    for (i = 0; i < items.length; i++) {
+        myTable += "<tr><td style='width: 100px; text-align: right;'>" + nums[i] + "</td>";
+        myTable += "<td style='width: 100px; text-align: right;'>" + items[i] + "</td>";
+        myTable += "<td style='width: 100px; text-align: right;'>" + prices[i] + "</td>";
+        myTable += "<td style='width: 100px; text-align: right;'>" + quantities[i] + "</td>";
+        myTable += "<td style='width: 100px; text-align: right;'>" + totals[i] + "</td>";
+        myTable += "<td><button onclick='add_selection(" + i + ")'>Add</button></td>";
+        myTable += "<td><button onclick='minus_selection(" + i + ")'>Minus</button></td>";
+    }
+	myTable += "<td><button onclick='display_Total()'>Calculate</button></td>";
+    myTable += "</table>";
+    // myTable += "<br/><br/><p>Total: " + totalOrderAmt + "</p>";
+
+
+
+    // document.write(myTable);
+    document.getElementById("demo").innerHTML = myTable;
+
+
+}
+
+window.onload = function() {
+    display_all();
+}
